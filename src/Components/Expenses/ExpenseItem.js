1eae463a738d7eb0dfe64d23cb78fd171a8972ab
NarendraDhamecha <nikhildhamecha19@gmail.com>
2022-12-31 20:58:44 +0530

    adding_delete_button

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -3,10 +3,16 @@ import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 
 const ExpenseItem = (props) => {
+  
+  const deleteEle = (id) => {
+      document.getElementById(id).remove()
+  } 
+
   return (
-    <div className="expense-item">
+    <div id={props.id} className="expense-item">
       <ExpenseDate date={props.date} />
       <ExpenseDetails title={props.title} amount={props.amount} />
+      <button onClick={(e) => {deleteEle(props.id, e)}}>Delete</button>
       {/* <div>{props.location}</div> */}
     </div>
   );
