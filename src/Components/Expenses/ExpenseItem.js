81e5dfb6bb0bf809e1477a099122068964da2da9
NarendraDhamecha <nikhildhamecha19@gmail.com>
2023-01-01 21:06:37 +0530

    react_state

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -1,9 +1,16 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
+import { useState } from "react";
 
 const ExpenseItem = (props) => {
   
+  const [value, updateValue] = useState(props.amount)
+
+  const changeExpense = () => {
+      updateValue('100')
+  }
+
   const deleteEle = (id) => {
       document.getElementById(id).remove()
   } 
@@ -11,7 +18,8 @@ const ExpenseItem = (props) => {
   return (
     <div id={props.id} className="expense-item">
       <ExpenseDate date={props.date} />
-      <ExpenseDetails title={props.title} amount={props.amount} />
+      <ExpenseDetails title={props.title} amount={value} />
+      <button onClick={changeExpense}>update</button>
       <button onClick={(e) => {deleteEle(props.id, e)}}>Delete</button>
       {/* <div>{props.location}</div> */}
     </div>
