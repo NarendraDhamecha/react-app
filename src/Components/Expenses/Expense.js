3f4f3fc135a8efedcddb84ad7beaf52cfa2af0ac
NarendraDhamecha <nikhildhamecha19@gmail.com>
2023-01-04 21:26:01 +0530

    conditional_rendering

--- src/Components/Expenses/Expense.js
+++ src/Components/Expenses/Expense.js
@@ -5,28 +5,50 @@ import "./Expense.css";
 import ExpenseFilter from "./ExpenseFilter";
 
 const Expense = (props) => {
-
   const [filteredValue, changeFilter] = useState("2020");
 
   const changeYear = (year) => {
     changeFilter(year);
   };
 
+  const filteredYear = props.items.filter(
+    expense => expense.date.getFullYear() === parseInt(filteredValue)
+  );
+
+  let expensesContent = <p>No expenses found</p>;
+
+  let msg = <p>Only single Expense here. Please add more...</p>;
+
+ if(filteredYear.length==1){
+    expensesContent = filteredYear.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+        msg={msg}
+      />
+    ))
+  }
+
+  else if(filteredYear.length > 0){
+      expensesContent = filteredYear.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))
+  }
+
   return (
-  <div>
-    <Card className="expenses">
-      <ExpenseFilter selected={filteredValue} onChangeFilter={changeYear} />
-      {props.items.filter(x => x.date.getFullYear() === parseInt(filteredValue))
-      .map(expense => 
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      )}
-    </Card>
-  </div> 
+    <div>
+      <Card className="expenses">
+        <ExpenseFilter selected={filteredValue} onChangeFilter={changeYear} />
+        {expensesContent}
+      </Card>
+    </div>
   );
 };
 
--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -1,28 +1,32 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
-import { useState } from "react";
+// import { useState } from "react";
+// import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
   
-  const [value, updateValue] = useState(props.amount)
+  // const [value, updateValue] = useState(props.amount)
 
-  const changeExpense = () => {
-      updateValue('100')
-  }
+  // const changeExpense = () => {
+  //     updateValue('100')
+  // }
 
-  const deleteEle = (id) => {
-      document.getElementById(id).remove()
-  } 
+  // const deleteEle = (id) => {
+  //     document.getElementById(id).remove()
+  // } 
 
   return (
+   <>
     <div id={props.id} className="expense-item">
       <ExpenseDate date={props.date} />
-      <ExpenseDetails title={props.title} amount={value} />
-      <button onClick={changeExpense}>update</button>
+      <ExpenseDetails title={props.title} amount={props.amount} />
+      {/* <button onClick={changeExpense}>update</button>
       <button onClick={(e) => {deleteEle(props.id, e)}}>Delete</button>
-      {/* <div>{props.location}</div> */}
+      <div>{props.location}</div> */}
     </div>
+    {props.msg}
+   </>
   );
 }
 
